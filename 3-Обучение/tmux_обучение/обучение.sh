10616af81beea223e624adb2dcd21f0af97e5643
Sabir777 <sabir.yanin2014@yandex.ru>
2024-08-03 18:44:18 +0300

    Изменил команду make_cpp: теперь шаблоны хранятся в папке Data. Изменил сессию "обучение" - теперь эта общий проект Training

--- 3-Обучение/tmux_обучение/обучение.sh
+++ 3-Обучение/tmux_обучение/обучение.sh
@@ -9,16 +9,16 @@ fi
 
 
 # Задаю текущую директорию tmux
-cd ~/Projects/Обучение
+cd ~/Projects/Training
 
 
 # Запускаю 1-е окно: Python
 # создаю сессию "Training" и окно "python"
 tmux new-session -s Training -n python -d
 
-# Запускаю виртуальное окружение indi-env
+# Запускаю виртуальное окружение venv_training
 # Открываю в vim "main.py"
-tmux send-keys -t Training 'source indi-course/indi-env/bin/activate ; cd indi-course ; vim main.py' C-m
+tmux send-keys -t Training 'source ~/Data/venv_training/bin/activate ; cd ~/Projects/Training/Python ; vim main.py' C-m
 
 
 # Запускаю 2-е окно: C++
@@ -26,14 +26,14 @@ tmux send-keys -t Training 'source indi-course/indi-env/bin/activate ; cd indi-c
 tmux new-window -n C++ -t Training
 
 # Открываю в vim "main.cpp"
-tmux send-keys -t Training:2.1 'cd cpp/src ; vim main.cpp' C-m
+tmux send-keys -t Training:2.1 'cd Cpp/src ; vim main.cpp' C-m
 
 # Запускаю 3-е окно: lua
 # создаю окно "lua"" в сессии Training
 tmux new-window -n lua -t Training
 
 # Открываю в vim "main.lua"
-tmux send-keys -t Training:3.1 'cd lua ; vim main.lua' C-m
+tmux send-keys -t Training:3.1 'cd Lua ; vim main.lua' C-m
 
 # Подключаюсь к сессии "Training"      
 tmux attach -t Training
