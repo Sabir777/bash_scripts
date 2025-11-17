0bf96edc0cc9712b3ca8d5e6d03712ca91676125
Sabir777 <sabir.yanin2014@yandex.ru>
2025-11-17 17:15:20 +0300

    Сделал мини-скрипт который открывает папку в которой находится файл

---
+++ 6-termux-widget/поиск/open_dir.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/env bash
+
+# Получаю имя файла или папки
+name="/data/data/com.termux/files/home/storage/shared/DriveSyncFiles/$*"
+
+if [[ -f "$name" ]]; then
+  name="${name%/*}"
+fi
+
+# Удаляю начало в имени
+name="${name#*home}"
+
+# Заменяю имя
+name="${name/shared/emulated\/0}"
+
+am start -a android.intent.action.VIEW -d "file:///$name" -t "resource/folder"
+# am start -a android.intent.action.VIEW -d file:///storage/emulated/0/DriveSyncFiles -t "resource/folder"
